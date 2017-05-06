97d2830e148f516bb8d226af10739d2acd1851fd
Javier Cabezas <jcleyton@gmail.com>
2017-05-06 20:11:12 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h3> asdfsaf </h3>
+
+    <p> holi </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
