e200ccda7f828b872c80b64717ab5ac6075e751f
Javier Cabezas <jcleyton@gmail.com>
2017-05-07 15:44:38 -0300

    Usage of router links to save the active class on the links

---
+++ src/components/top/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-default navbar-static-top yamm">
+        <div class="container">
+            <div class="navbar-header col-sm-12 col-md-6 col-lg-4">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                    <span class="sr-only">MassiveWave</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="../../img/mw-logo2.png" alt="logo"></a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><router-link to="/">Home</router-link></li>
+                    <li><router-link to="/about">About</router-link></li>
+                    <li><router-link to="/music-packs"> Music Packs </router-link></li>
+                    <li><router-link to="/faq">FAQ</router-link></li>
+                    <li><router-link to="/blog">Blog</router-link></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ src/components/top/TopBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-right">
+                    <ul class="list-inline">
+                        <li class="hidden-xs"><a href="#/register"><i class="fa fa-user"></i> Account </a></li>
+                        <li><a href="#/login"><i class="pe-7s-lock"></i> Login </a></li>
+                        <li class="lang-dropdown">
+                            <a> English </a>
+                            <div class="lang-drop-menu">
+                                <a><img src="../../img/flag_en.png"> English </a>
+                                <a><img src="../../img/flag_es.png"> Spanish </a>
+                            </div>
+                        </li>
+                        <li><a href="javascript:void(0)" class="search-toggle"><i class="fa fa-search"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
