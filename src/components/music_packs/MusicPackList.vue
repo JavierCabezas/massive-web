6cede948ee02cdcb6619e3a5a1221af821fc364f
Javier Cabezas <jcleyton@gmail.com>
2017-05-14 11:55:54 -0400

    Working pagination for music pack section

--- src/components/music_packs/MusicPackList.vue
+++ src/components/music_packs/MusicPackList.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="row">
-        <div class="col-sm-4" v-for="item in items">
-            <div class="item_holder">
-                <a :href="'#/music-pack/'+item.id"><img :src="item.img" alt="" class="img-responsive"></a>
+
+        <div class="col-sm-4" v-for="(item, index) in items">
+            <div class="item_holder" v-if="is_index_in_range(index)">
+                <a :href="'#/music-pack/'+item.id"><img :src="item.img" :alt="item.title" class="img-responsive"></a>
                 <div class="title">
-                    <h5>{{item.title}}</h5>
+                    <h5>{{index}} : {{item.title}}</h5>
                     <span class="price">{{item.price}}</span>
                     <p>{{item.text}}</p>
                 </div>
             </div>
         </div>
         <paginate
-                :pageCount="10"
+                :pageCount="computedItems.length / items_per_page"
                 :containerClass="'pagination'"
                 :clickHandler="clickCallback">
         </paginate>
@@ -23,12 +24,29 @@
 
     export default {
         props: [ 'items' ],
+        computed: {
+            computedItems: function () {
+                return  ( this.items != null ) ?  this.items : [];
+            }
+        },
+        data () {
+            return {
+                items_per_page: 12,
+                page_count: 3,
+                current_page: 1
+            }
+        },
         components: {
             paginate: Paginate
         },
         methods: {
             clickCallback: function(page) {
-                console.log(page)
+                this.current_page = page;
+                console.log(this.computedItems);
+            },
+            is_index_in_range(index){
+                let item_number = index +1;
+                return index > ( (this.current_page -1 ) * this.items_per_page ) && index <= ( (this.current_page ) * this.items_per_page)
             }
         }
     }
