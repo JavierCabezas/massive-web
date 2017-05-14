1f5710d543f197ed2cfb6097945dd77bab13b315
Javier Cabezas <jcleyton@gmail.com>
2017-05-14 18:35:42 -0400

    Blog posts dummy backend and usage in views

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -213,4 +213,43 @@ class SiteController extends Controller
         return $out;
     }
 
+    public function actionBlogPost(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $titles = [ "Four loko", "Fixie nostrud", "Master Cleanse", "Scenester hell", "Shabby chic", "Blue bottle", "Duis pitchfork", "Raclette portland", "Cardigan glossier", "Tumeric keffiyeh" ];
+        $img_root = 'http://localhost/massive-web/src/img/blog/';
+        $text = [
+            "<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex</p>",
+            "<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+            <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+            <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally</p>",
+            "<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to</p>",
+            "<p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>
+            <p>The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
+            <p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. \"What's happened to me? \" he thought. It wasn't a dream. His room, a proper human</p>"
+        ];
+        shuffle($text);
+        shuffle($titles);
+        $categories = [ 'News', 'Updates', 'Otro' ];
+        $categories_to_add = [];
+        for($j = 0; $j <= rand(1, 2) ; $j+= 1){
+            shuffle($categories);
+            array_push($categories_to_add, $categories[0]);
+        }
+
+        return [
+            'title' => $titles[0],
+            'summary' => $text[0],
+            'img' => $img_root.'00'.rand(1, 6).'.jpg',
+            'tags' => $categories_to_add,
+            'info' => 'Text'
+        ];
+    }
+
 }
--- src/components/sections/BlogPost.vue
+++ src/components/sections/BlogPost.vue
@@ -1,6 +1,5 @@
 <template>
     <section>
-        <p>Post ID: {{id}} </p>
         <div class="space-60"></div>
         <div class="container">
             <div class="row">
@@ -8,26 +7,22 @@
                     <a href="#">
                         <img src="" class="img-responsive" alt="">
                     </a>
-                    <div class="blog-desc">
-                        <a href="" class="tag"><i class="fa fa-tag"></i> </a>
-                        <h4 class="title"><a href="#"> {{post.title}} </a></h4>
-                        <p> {{post.summary}}</p>
-                        <p class="lead">{{post.info}}</p>
 
-                    </div><!--blog desc-->
-                    <div class="comments">
-                        <h3><i class="pe-7s-comment"></i></h3>
-                        <div class="media">
-                            <div class="media-left">
-                                <img class="media-object img-circle" width="60" src="../../img/team-1.jpg" alt="...">
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">John Doe <a href="#">Reply</a></h4>
-                                <p>
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-                                </p>
-                            </div>
+                    <div class="blog-desc">
+
+                        <h4 class="title"><a href="#"> {{post.title}} </a></h4>
+                        <img :src="post.img" class="img-responsive" alt="">
+                        <p v-html="post.summary"> </p>
+
+                        <div class="categories">
+                            <a href="" class="tag"><i class="fa fa-tag"></i> </a>
+                            <ul>
+                                <li v-for="cat in post.tags">
+                                    {{cat}}
+                                </li>
+                            </ul>
                         </div>
+                        
                     </div>
 
                     <a href="/#/blog"> Back to posts </a>
@@ -46,18 +41,22 @@
                 watch: {
                     '$route'(to, from){
                         this.id = params.id;
-                        //To-do: Do an ajax call to update the blog post
                     }
                 },
-                post: {
-                        id: 1,
-                        title: "I mean, they have no respect for us up there! No respect whatsoever!",
-                        summary: "We're all just drudgeons to them! Unbelievable! Some idiot disabled his firewall, meaning all the computers on Seven are teeming with ... ",
-                        info: "Plus I've just had to walk all the way down the motherfudging stairs, because the lifts are broken AGAIN! It's my term for my time of the month. Oh. What time of the month? The weekend? So, remember the new number! 0118 999! 88199, 9119 725! ... 3! Hello, IT. Have you tried turning it off and on again? A gay musical, called Gay. That's quite gay. Gay musical? Aren't all musicals gay? This must be, like, the gayest musical ever. <br> Oh my God. I didn't even know Smarties made a cereal. They don't. It's just Smarties in a bowl with milk. You're not comfortable with your sexuality? Oh, I'm very comfortable with my sexuality, I just don't want to be slapped in the face with their sexuality. So, remember the new number! 0118 999! 88199, 9119 725! ... 3! I mean, they have no respect for us up there! No respect whatsoever! We're all just drudgeons to them! Today I have a business empire the like of which the world has never seen the like of which. I hope it doesn't sound arrogant when I say that I am the greatest man in the world! No, no, that's the music you heard when it come on.",
-                        comments: '1 comment',
-                        tags: [ 'tag1', 'tag5']
-                }
+                post: []
             }
-        }
+        },
+        created: function () {
+            let vm = this;
+            let url = vm.url_backend + '/site/blog-post';
+
+            $.ajax({
+                url: url,
+                data: { id: this.$route.params.id },
+                success: function (result) {
+                    vm.post = result;
+                }
+            });
+        },
     }
 </script>
