3041e27e78dd9b0e12bde7e4dbd05211bf5e00a2
Fernanda Kauak <fernanda.kauak@gmail.com>
2017-07-23 21:05:00 -0400

    Testing repeats

--- src/components/music_packs/search/MoodSelector.vue
+++ src/components/music_packs/search/MoodSelector.vue
@@ -1,30 +1,20 @@
 <template>
-    <div>
+    <div class="row">
         <section class="col-sm-offset-1 col-sm-10 option-search">
             <h3>Choose an Option!</h3>
-            <a href="#" @click.prevent="show_content('music_type')"><div class="col-sm-12 col-md-6 option">Positivo</div></a>
-            <a href="#" @click.prevent="show_content('music_type')"><div class="col-sm-12 col-md-6 option">Negativo</div></a>
-            <a href="#" @click.prevent="show_content('music_type')"><div class="col-sm-12 col-md-6 option">Activo</div></a>
-            <a href="#" @click.prevent="show_content('music_type')"><div class="col-sm-12 col-md-6 option">Pasivo</div></a>
+            <a href="#" @click.prevent="show_content('music_type')" v-for="item in mood">
+                <div class="col-sm-12 col-md-6 option">{{item.type}}</div>
+            </a>
         </section>
 
-        <div v-if="content_to_show == 'music_type'">
-            <section class="col-sm-offset-1 col-sm-10 musictype-search">
+        <transition name="fade">
+            <section v-if="content_to_show == 'music_type'" class="col-sm-offset-1 col-sm-10 musictype-search">
                 <h3>Choose a Music Style!</h3>
-                <a href="#" @click.prevent="get_parent_products(1)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(2)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(3)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(4)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(5)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(6)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(7)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(8)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(9)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(10)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(11)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
-                <a href="#" @click.prevent="get_parent_products(12)"><div class="col-sm-12 col-md-2 option">Music Type</div></a>
+                <a href="#" @click.prevent="get_parent_products(1)" v-for="item in genre">
+                    <div class="col-sm-12 col-md-2 option">{{item.type}}</div>
+                </a>
             </section>
-        </div>
+        </transition>
     </div>
 </template>
 
@@ -32,7 +22,59 @@
     export default {
         data () {
             return {
-                content_to_show: " "
+                content_to_show: " ",
+                mood: [
+                    {
+                        type: 'Positivo'
+                    },
+                    {
+                        type: 'Negativo'
+                    },
+                    {
+                        type: 'Activo'
+                    },
+                    {
+                        type: 'Pasivo'
+                    }
+                ],
+                genre: [
+                    {
+                        type: 'Genre 1'
+                    },
+                    {
+                        type: 'Genre 2'
+                    },
+                    {
+                        type: 'Genre 3'
+                    },
+                    {
+                        type: 'Genre 4'
+                    },
+                    {
+                        type: 'Genre 5'
+                    },
+                    {
+                        type: 'Genre 6'
+                    },
+                    {
+                        type: 'Genre 7'
+                    },
+                    {
+                        type: 'Genre 8'
+                    },
+                    {
+                        type: 'Genre 9'
+                    },
+                    {
+                        type: 'Genre 10'
+                    },
+                    {
+                        type: 'Genre 11'
+                    },
+                    {
+                        type: 'Genre 12'
+                    },
+                ]
             }
         },
         methods:{
@@ -45,3 +87,12 @@
         }
     }
 </script>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0
+    }
+</style>
