8c6507f8fcef075b96f3ddd22bd7e9a4d127a547
Javier Cabezas <jcleyton@gmail.com>
2017-10-11 15:32:53 -0300

    Modified backend to give optional param for limit of elements to posts

--- backend/massive_web/blogs/urls.py
+++ backend/massive_web/blogs/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^posts/(?P<limit>\d+)/$', views.posts, name='posts_with_limit'),
     url(r'^posts', views.posts, name='posts'),
     url(r'^post/(?P<id>\d+)/$', views.post, name='post')
 ]
--- backend/massive_web/blogs/views.py
+++ backend/massive_web/blogs/views.py
@@ -2,10 +2,13 @@ from django.http import JsonResponse
 
 from .models import Blog
 
-import random
 
-def posts(request):
-    return JsonResponse([b.backend() for b in Blog.objects.all()], safe=False)
+def posts(request, limit=None):
+    if limit is None:
+        return JsonResponse([b.backend() for b in Blog.objects.all()], safe=False)
+    else:
+        return JsonResponse([b.backend() for b in Blog.objects.all()[:int(limit)]], safe=False)
+
 
 def post(request, id):
     b = Blog.objects.get(id=id)
