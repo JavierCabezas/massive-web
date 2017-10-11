a2783c681df3c3ecbaa2b41eab33d0fce5acea82
Javier Cabezas <jcleyton@gmail.com>
2017-10-11 11:48:12 -0300

    Fixed breadcrumbs and links on blog post

--- src/components/sections/BlogPost.vue
+++ src/components/sections/BlogPost.vue
@@ -5,30 +5,42 @@
                 <h1>{{post.name}}</h1>
             </div>
         </div>
+        <bread-crumbs :crumbs="crumbs"></bread-crumbs>
+
         <div class="row">
             <div class="blog-item blog-content col-sm-10 col-sm-offset-1">
                 <div class="blog-desc">
                     <img :src="post.image" class="img-responsive featured-blog" alt="">
                     <p class="whitespace"> {{post.post}} </p>
                     <div class="categories">
-                        <a href="" class="tag"><i class="fa fa-tag"></i> </a>
+                        <p>Tags</p>
                         <ul>
                             <li v-for="tag in post.tags">
-                                {{tag.name}}
+                                <i class="fa fa-tag"></i>  {{tag.name}}
                             </li>
                         </ul>
                     </div>
 
                 </div>
 
-                <a href="/#/blog"> Back to posts </a>
+                <router-link tag="a" :to="{name:'blog'}"> {{ t('back') }} </router-link>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import BreadCrumbs from '../main/Breadcrumbs.vue'
+
     export default{
+         locales: {
+            es_ES: {
+                back: 'Volver a los posts',
+            },
+            en_US: {
+                back: 'Back to posts',
+            }
+        },
         data() {
             return {
                 id: this.$route.params.id,
@@ -37,8 +49,29 @@
                         this.id = params.id;
                     }
                 },
+                crumbs: {
+                    nav: [
+                        {
+                            link: 'home',
+                            name_en: 'Home',
+                            name_es: 'Inicio'
+                        },
+                        {
+                            link: 'blog',
+                            name_en: 'Blog',
+                            name_es: 'Blog'
+                        }
+                    ],
+                    current: {
+                        es: 'Post',
+                        en: 'Post'
+                    }
+                },
                 post: []
             }
+        },
+        components: {
+            BreadCrumbs
         },
         created: function () {
             let vm = this;
