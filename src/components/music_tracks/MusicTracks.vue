19054b59e3313e4f33b8328067c0117a7dc05ce1
Javier Cabezas <jcleyton@gmail.com>
2017-10-14 23:16:51 -0300

    Filter music tracks by category

--- src/components/music_tracks/MusicTracks.vue
+++ src/components/music_tracks/MusicTracks.vue
@@ -8,9 +8,13 @@
         <bread-crumbs :crumbs="crumbs"></bread-crumbs>
         <div class="space-60"></div>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8">
                 <music-track-list :items="items"></music-track-list>
             </div>
+            <music-categories :categories="categories"
+                              :selected_category_index="selected_category_index"
+                              @category_changed="get_music_tracks"
+            ></music-categories>
         </div>
     </div>
 </template>
@@ -18,11 +22,14 @@
 <script>
     import MusicTrackList from './MusicTrackList.vue'
     import BreadCrumbs from '../main/Breadcrumbs.vue'
+    import MusicCategories from './../music_packs/MusicPacksCategories.vue'
 
     export default {
         data () {
             return {
                 items: [ ],
+                categories: {},
+                selected_category_index: -1,
                 crumbs: {
                     nav: [
                         {
@@ -39,19 +46,43 @@
             }
         },
         created: function () {
-            let vm = this;
-            let url = vm.url_backend + 'music_track/index';
-
-            $.ajax({
-                url: url,
-                success: function (result) {
-                    vm.items = result;
+            this.get_categories();
+            this.get_music_tracks();
+        },
+        methods: {
+            get_music_tracks: function(category_index = -1) {
+                let vm = this;
+                let url = vm.url_backend + 'music_track/index';
+                let category_id = -1;
+                if(category_index > -1){
+                    category_id = vm.categories[category_index].id;
+                    vm.selected_category_index = category_index;
+                }else{
+                    vm.selected_category_index = -1;
                 }
-            });
+
+                $.ajax({
+                    url: url,
+                    data: { category_id: category_id },
+                    success: function (result) {
+                        vm.items = result;
+                    }
+                });
+            },
+            get_categories: function(){
+                let vm = this;
+                $.ajax({
+                    url: vm.url_backend + 'categories/parents',
+                    success: function (result) {
+                        vm.categories = result;
+                    }
+                });
+            }
         },
         components: {
-            musicTrackList: MusicTrackList,
-            breadCrumbs: BreadCrumbs
+            MusicTrackList,
+            MusicCategories,
+            BreadCrumbs
         }
     }
 </script>
