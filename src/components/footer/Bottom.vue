4cdbcfe8f9b241d7b393e3f9ff5d033ac144096a
Javier Cabezas <jcleyton@gmail.com>
2017-10-15 18:19:57 -0300

    Fixed footer

--- src/components/footer/Bottom.vue
+++ src/components/footer/Bottom.vue
@@ -2,14 +2,10 @@
     <footer class="footer-bottom">
         <div class="container text-center">
             <div class="logo col-sm-12 col-md-offset-4 col-md-4">
-                <a href="index.html"><img src="../../img/mw-logo2.png" alt=""></a>
+                <router-link tag="a" :to="{name: 'home'}">
+                    <img src="../../img/mw-logo2.png" alt="">
+                </router-link>
             </div>
-            <ul class="list-inline col-sm-12 col-md-offset-3 col-md-6">
-                <li><a href="#">Sitemap</a></li>
-                <li><a href="#/about">About</a></li>
-                <li><a href="#/blog">Blog</a></li>
-                <li><a href="#/privacy">Privacy</a></li>
-            </ul>
             <img src="../../img/payment.png" alt="" class="img-responsive payment">
             <span class="copyright col-sm-12 col-md-offset-3 col-md-6">Copyright 2016 &copy; MassiveWave</span>
             <ul class="redes-footer clearfix list-inline text-center">
