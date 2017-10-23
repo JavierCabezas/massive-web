8b6d1ea0fdc26979fe91b4dbda709a4e45aed316
Javier Cabezas <jcleyton@gmail.com>
2017-10-22 22:30:44 -0300

    Changes on migrations on the cart model

--- backend/massive_web/payment/models.py
+++ backend/massive_web/payment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from ..users.models import User
+from ..music_packs.models import MusicPack
+from ..music_tracks.models import MusicTrack
 
-# Create your models here.
+
+class Cart(models.Model):
+    creation_date = models.DateTimeField(
+        null=False
+    )
+
+    payment_date = models.DateTimeField(
+        null=True
+    )
+
+    deleted_date = models.DateTimeField(
+        null=True
+    )
+
+    amount = models.FloatField(
+        null=False
+    )
+
+    user = models.ForeignKey(User)
+
+    music_packs = models.ManyToManyField(MusicPack)
+
+    music_tracks = models.ManyToManyField(MusicTrack)
